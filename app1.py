1946532de9024112e8485b4a6fb7be529bd506a8
Tanya <168749225+tanu-1403@users.noreply.github.com>
2025-10-05 10:42:50 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -13,7 +13,7 @@ st.markdown("""
 
 .stApp {
     /* Background image placeholder: Add your own image URL below */
-    background-image: url("");
+    background-image: url("Terror-Attacks~2.jpg");
     background-size: cover;
     background-position: center;
     background-attachment: fixed;
@@ -189,3 +189,4 @@ if not dff.empty:
     """, unsafe_allow_html=True)
 else:
     st.info("No data available for the selected filters.")
+
