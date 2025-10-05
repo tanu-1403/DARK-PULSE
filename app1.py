e2ba5e6fc36b4fedcf8ba369373ccf9eb110fa21
Tanya <168749225+tanu-1403@users.noreply.github.com>
2025-10-05 10:48:25 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -13,7 +13,7 @@ st.markdown("""
 
 .stApp {
     /* Background image placeholder: Add your own image URL below */
-    background-image: url("https://github.com/tanu-1403/DARK-PULSE/blob/main/Terror-Attacks~2.jpg");
+    background-image: url(https://raw.githubusercontent.com/tanu-1403/DARK-PULSE/main/Terror-Attacks~2.jpg);
     background-size: cover;
     background-position: center;
     background-attachment: fixed;
@@ -189,5 +189,6 @@ if not dff.empty:
     """, unsafe_allow_html=True)
 else:
     st.info("No data available for the selected filters.")
+
 
 
